5a76c7d58a73ff1146181dc8c1d92dc16c31349f
zn11ch <nehaivova@ya.ru>
2024-05-22 10:36:30 +0300

    Add tabs for restaurants

--- src/components/restaraunts/component.jsx
+++ src/components/restaraunts/component.jsx
@@ -1,11 +1,28 @@
 import { Restarunt } from "../restaraunt/component";
+import { useState } from "react";
 
 export const Restarunts = ({ restaurants }) => {
+  const [activeRestaurantIndex, setActiveRestaurantIndex] = useState(0);
+
+  if (!restaurants || !restaurants?.length) {
+    return null;
+  }
+
   return (
     <div className="restaraunts">
-      {restaurants.map((restaurant) => (
-        <Restarunt key={restaurant.id} restaurant={restaurant} />
+      {restaurants.map((restaurant, restaurantIndex) => (
+        <button
+          key={restaurant.id}
+          onClick={() => setActiveRestaurantIndex(restaurantIndex)}
+          className={
+            activeRestaurantIndex === restaurantIndex ? "btn-active" : null
+          }
+        >
+          {restaurant.name}
+        </button>
       ))}
+
+      <Restarunt restaurant={restaurants[activeRestaurantIndex]} />
     </div>
   );
 };
