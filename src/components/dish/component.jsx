a1d98623a309c915174206f3fdbb0e8aea669dbf
zn11ch <nehaivova@ya.ru>
2024-05-26 21:12:48 +0300

    Add custom hook useCount

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
+import { useCount } from "../../hooks/use-count"
 import { Counter } from "../counter/component";
 import { Ingredients } from "../ingredients/component";
 const initialValue = 0;
 
 export const Dish = ({ dish }) => {
-  const [count, setCount] = useState(initialValue);
+  const {count, increment, decrement} = useCount(initialValue);
+
   return (
-    <>
+    <div>
       <h4>{dish.name}</h4>
-      <Counter value={count} onChange={setCount} />
-      {count * dish.price}
+      <Counter value={count} increment={increment} decrement={decrement} />
+      <div>{count * dish.price}</div>
       <Ingredients ingredients={dish.ingredients} />
-    </>
+    </div>
   );
 };
